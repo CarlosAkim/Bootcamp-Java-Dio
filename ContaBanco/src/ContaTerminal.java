2ee62530764dfba148efbe08916fe709fdc3df2a
Carlos Akim <Ch.akim1994@gmail.com>
2025-01-13 11:57:02 -0300

    Atualização do projeto

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,6 +1,53 @@
+import java.util.Scanner ;
 
 public class ContaTerminal {
-	public static void main(String[] args) {
-		System.out.println("Hello world");
+	public static void main(String[] args){
+		ContaUsuario cliente = new ContaUsuario();
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		try {
+			System.out.println("Digite o nome do Cliente: ");
+			cliente.setCliente(scanner.nextLine());	
+			
+			System.out.println("Digite a agencia (formato 123-4): ");
+			cliente.setAgencia(scanner.nextLine());
+			
+			//Garantia que não seja adicionado nem um valor além do INT
+			while(true) {
+				try{
+					System.out.println("Digite o numero da conta: ");
+					cliente.setNumero(scanner.nextInt());
+					break;
+				}catch (Exception e) {
+					System.out.println("Entrada invalida ");
+					scanner.nextLine();
+				}
+			}
+			
+			//Garantia que não seja adicionado um outro valor além do double
+			while(true) {
+				try {
+					System.out.println("Digite o saldo que gostaria de depositar: ");
+					cliente.setSaldo(scanner.nextDouble());
+					break;
+				}catch (Exception e){
+					System.out.println("Entrada invalida ");
+					scanner.nextLine();
+				}
+			}
+			
+			
+		}catch (Exception e) {
+			System.out.println("Erro :" + e.getMessage());
+		}finally {
+			scanner.close();
+		}
+		
+		System.out.println("Olá cliente " + cliente.getCliente() + " obrigado por criar uma conta em nosso "
+				+ "banco, sua agencia é " + cliente.getAgencia() + " conta " + cliente.getNumero() + " e seu saldo "
+				+ cliente.getSaldo() + " está disponivel para saque");
+		
+		
 	}
 }
---
+++ ContaBanco/src/ContaUsuario.java
@@ -0,0 +1,39 @@
+
+public class ContaUsuario {
+	int numero;
+	String agencia;
+	String cliente;
+	double saldo;
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(String agencia) {
+		this.agencia = agencia;
+	}
+
+	public String getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(String cliente) {
+		this.cliente = cliente;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+}
